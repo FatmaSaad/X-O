eeb84623a8d00d57b4d71e3ed5828e54da3b0565
AshrafAmer <ashraf.amer55@gmail.com>
2020-01-13 15:44:08 +0200

    Server last Edit

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -104,34 +104,45 @@ class UserCheck extends Thread
                 loginUser = dis.readLine();
                 String[] arr = loginUser.split(",");
 
-		String loginPhone = arr[0];
-                String loginPass = arr[1];
+                String state = arr[0];
                 
-                String query = "SELECT * FROM users";
-                ResultSet res = stmt.executeQuery(query);
+                if(state.compareTo("login") == 0) // Data come from login Form
+                {
+                    String loginPhone = arr[1];
+                    String loginPass = arr[2];
+                    String query = "SELECT * FROM users";
+                    ResultSet res = stmt.executeQuery(query);
                   
-                while(!res.isLast())
-                {
-                    if(res.next())
+                    while(!res.isLast())
                     {
-                        if(res.getString(5).compareTo(loginPhone) == 0)
+                        if(res.next())
                         {
-                            isExist = true; // yes it is registered
+                            if(res.getString(5).compareTo(loginPhone) == 0)
+                            {
+                                isExist = true; // yes it is registered
 
-                            if(res.getString(4).compareTo(loginPass) == 0)
-                               passTrue = true; // Check entered new pass or not
-                        }                               
+                                if(res.getString(4).compareTo(loginPass) == 0)
+                                   passTrue = true; // Check entered new pass or not
+                            }                               
+                        }
                     }
-                }
 
-                if(isExist && passTrue)
-                {
-                    String quer = "update users set state = 'online'" + " Where phone =" + loginPhone;
-                    stmt.executeUpdate(quer);
+                    if(isExist && passTrue)
+                    {
+                        String updateState = "update users set state = 'online'" + " Where phone =" + loginPhone;
+                        stmt.executeUpdate(updateState);
+                    }
+                    ps.println(isExist + "," + passTrue);
+                    System.out.println(loginPhone);
+                    System.out.println(loginPass);
                 }
-                ps.println(isExist + "," + passTrue);
-                System.out.println(loginPhone);
-                System.out.println(loginPass);
+                else if(state.compareTo("register") == 0) // Data come from Register Form
+                {
+                    String addNewUser = "INSERT INTO users(username, email, password, phone, gender, img_url, total_score, state) VALUES('"+arr[1]+"', '"+arr[2]+"', '"+arr[3]+"', '"+arr[4]+"', '"+arr[5]+"', 'image url', 0, 'online')";
+                    stmt.executeUpdate(addNewUser);
+                    ps.println("registred");
+                    System.out.println("registred");
+                }
         
             } catch (IOException | SQLException ex) {
                 Logger.getLogger(UserCheck.class.getName()).log(Level.SEVERE, null, ex);
