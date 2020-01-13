1290d0118b8c37a0b3e0e250d0aeb20489ba12a6
nora elshahat <nelshohat@gmail.com>
2020-01-13 12:18:36 +0200

    edit loin class

--- src/x/o/XO.java
+++ src/x/o/XO.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class XO extends Application {    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = new PlayingModes(stage);
+        Parent root = new Login(stage);
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
